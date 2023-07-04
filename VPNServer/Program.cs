3a1eab3e937e1b631d16e3c2ef6cf3fa7049b94c
mon1tor <westxlebyshek@gmail.com>
2023-07-04 21:38:18 +0300

    Add project files.

---
+++ VPNServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace VPNServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "bash",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                }
+            };
+            process.Start();
+            await process.StandardInput.WriteLineAsync("echo pass | sudo -S wg");
+
+            string? output;
+            while ((output = await process.StandardOutput.ReadLineAsync()) != null)
+            {
+                Console.WriteLine($"{output}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
