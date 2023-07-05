b5eb8407fdc54eedb7eb02be380b466b938e36ab
mon1tor <westxlebyshek@gmail.com>
2023-07-06 01:39:20 +0300

    Wg command output parsing

---
+++ VPNServer/Classes/Peer.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using VPNServer.Utils;
+
+namespace VPNServer.Classes
+{
+    internal class Peer
+    {
+        public byte[] PublicKey { get; set; }
+        public IPAddress AllowedIPs { get; set; }
+        public DateTime LatestHandshakeTimestamp { get; set; }
+
+        public bool EqualToPeer(Peer peer) => ProgramUtils.UnsafeCompare(PublicKey, peer.PublicKey);
+
+        public override string ToString() => $"Peer: {Convert.ToBase64String(PublicKey)}, {AllowedIPs}, {LatestHandshakeTimestamp}";
+    }
+}
--- VPNServer/Program.cs
+++ VPNServer/Program.cs
@@ -1,18 +1,20 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using VPNServer.Classes;
 using VPNServer.Utils;
 
 namespace VPNServer
 {
     public class Program
     {
+        private static List<Peer> ConnectedPeers = new();
+
         public static async Task Main(string[] args)
         {
             do
             {
                 IEnumerable<string> result = await ConsoleUtils.ExecuteCommand("wg");
-                foreach (string s in result)
-                    Console.WriteLine(s);
+                IEnumerable<Peer> peers = ParsingUtils.ParsePeersFromWG(result);
+                foreach(Peer peer in peers)
+                    Console.WriteLine(peer.ToString());
                 Thread.Sleep(1000);
             } while (true);
 
