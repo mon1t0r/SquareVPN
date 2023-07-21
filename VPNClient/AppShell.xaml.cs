c2d9813a66d17901a6ee9465877d7516cdc18381
mon1tor <westxlebyshek@gmail.com>
2023-07-21 15:23:07 +0300

    .NET MAUI VPN client project create

---
+++ VPNClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VPNClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VPNClient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VPNClient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
